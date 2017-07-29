987fe0fed6edef4347a9d3284cd0243edcced28a
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-07-29 18:34:30 -0500

    Enable migrations. Add Movie and Customer models.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek!"},
+                new Movie { Id = 2, Name = "Wall-E"}
+            };
+
+            return View(movies);
+        }
+    }
+}
