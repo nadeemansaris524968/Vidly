98af6718cc5a0a875759e099908f3cb674016464
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-07-30 01:11:50 -0500

    Populate Genres. Add Genres to Movies Index page.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -6,15 +7,28 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            var movies = new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek!"},
-                new Movie { Id = 2, Name = "Wall-E"}
-            };
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,11 +1,26 @@
-﻿namespace Vidly.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime? DateAdded { get; set; }
+
+        [Required]
+        public byte NumberInStock { get; set; }
+
         public byte GenreId { get; set; }
+
         public Genre Genre { get; set; }
     }
 }
