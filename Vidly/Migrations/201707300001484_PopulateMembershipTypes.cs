4dc732480653acbf73609d7e1d8b5e690c643fcd
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-07-29 19:06:31 -0500

    Add Newsletter subscription and Membership Type to Customer.

---
+++ Vidly/Migrations/201707300001484_PopulateMembershipTypes.cs
@@ -0,0 +1,19 @@
+namespace Vidly.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (1, 0, 0, 0)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (2, 30, 1, 10)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (3, 90, 3, 20)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (4, 300, 12, 30)");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
