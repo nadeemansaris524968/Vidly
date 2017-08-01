c3b978653019d9e9e1f611bcb2529f6f5dbd94be
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-07-31 20:46:32 -0500

    Add Automapper to project. Add CustomerDto to accept form data before storing in DB.

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
