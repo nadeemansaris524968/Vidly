697bbc9e597133b09efa752f9fb4e3d65d903613
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-08-06 03:22:23 -0500

    Add Typeahead.js to project.

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -13,6 +13,7 @@ namespace Vidly
                               "~/scripts/bootbox.js",
                               "~/scripts/datatables/jquery.datatables.js",
                               "~/scripts/datatables/datatables.bootstrap.js",
+                              "~/scripts/typeahead.bundle.js",
                               "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -26,6 +27,7 @@ namespace Vidly
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
                       "~/content/datatables/css/datatables.bootstrap.css",
+                      "~/content/typeahead.css",
                       "~/Content/site.css"));
         }
     }
