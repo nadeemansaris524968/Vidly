8b99d545ca438d809557669603f2ae9f2d3228dd
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-08-05 22:27:28 -0500

    Add Rentals model with associated Dto.
    Also add NewRentals Api Controller.

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -38,6 +38,8 @@ namespace Vidly.Models
 
         public DbSet<MembershipType> MembershipTypes { get; set; }
 
+        public DbSet<Rental> Rentals { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
